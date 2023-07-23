a91e74c471898a6ae90dae3c7d4080381db080e7
descartes640@gmail.com <descartes640@gmail.com>
2023-07-22 18:15:28 -0600

    Clasificacion RN2

---
+++ RN2.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from keras.models import Sequential 
+from keras.layers import Dense
+from keras.layers.core import activation
+
+
+class Clasificacion:
+    
+    
+    def __init__(self):
+        
+        self.df
+        self.x
+        self.y
+        self.model
+        self.
